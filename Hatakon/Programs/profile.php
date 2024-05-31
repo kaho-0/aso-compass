2063c113233a103641fd09f1c04228f2c9915059
n6-lu <2201130@s.asojuku.ac.jp>
2024-05-31 12:29:54 +0900

    Update profile.php

--- Hatakon/Programs/profile.php
+++ Hatakon/Programs/profile.php
@@ -28,7 +28,7 @@ $user = [
     <!--Bootstrap5のリンク-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!--navbarのリンク-->
-    <!-- <?php require 'navbar.php'; ?> -->
+    <?php require 'navbar.php'; ?>
     <!--style.cssに書き加えて、cssのファイル名を変更してください-->
     <link rel="stylesheet" href="../assets/css/profile.css">
 </head>
