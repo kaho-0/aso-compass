aa6acc75a22233f3d777803dd53618753a0db3dd
n6-lu <2201130@s.asojuku.ac.jp>
2024-06-28 10:57:39 +0900

    Update db-connect.php

--- Hatakon/Programs/db-connect.php
+++ Hatakon/Programs/db-connect.php
@@ -1,8 +1,16 @@
 <?php
-    const SERVER = 'mysql304.phy.lolipop.lan';
-    const DBNAME = 'LAA1517471-asocompas';
-    const USER = 'LAA1517471';
-    const PASS = 'asocompas';
+const SERVER = 'mysql304.phy.lolipop.lan';
+const DBNAME = 'LAA1517471-asocompas';
+const USER = 'LAA1517471';
+const PASS = 'asocompas';
 
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+$dsn = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+
+try {
+    $pdo = new PDO($dsn, USER, PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'データベース接続失敗: ' . $e->getMessage();
+    exit;
+}
 ?>
