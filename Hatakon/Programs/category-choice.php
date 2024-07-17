02b85477ff455d7272fe54afe514e1395b68cc31
kaho-0 <2201175@s.asojuku.ac.jp>
2024-07-17 10:06:00 +0900

    category-choice完成

--- Hatakon/Programs/category-choice.php
+++ Hatakon/Programs/category-choice.php
@@ -1,8 +1,41 @@
 <?php
 session_start(); // セッションの開始
 require 'db-connect.php'; // データベース接続
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asocompass</title>
+
+    <!--fontのリンク-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <!--iconのリンク-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="icon" href="../assets/image/favicon.ico">
+    <!--Bootstrap5のリンク-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- PHP関連読み込み -->
+    <?php require 'navbar.php'; ?> <!-- navbarのリンク -->
+    <!--style.cssに書き加えて、cssのファイル名を変更してください-->
+    <link rel="stylesheet" href="../assets/css/category-choice.css">
+    <link rel="stylesheet" href="../assets/css/profile.css">
+</head>
+<body>
+
+<?php
 $pdo = new PDO($connect, USER, PASS); // データベース接続
-$user_id = $_SESSION['acccount']['id']; // セッションユーザーのlike_idを取得
+if (isset($_SESSION['account']['id'])) {
+  $user_id = $_SESSION['account']['id'];
+} else {
+  // セッションユーザーが取得できない場合のエラーメッセージ
+  die('セッションが無効です。ログインしてください。');
+}
 
 
 // カテゴリーIDを取得
@@ -46,33 +79,7 @@ $cate_stmt = $pdo->prepare('
 $cate_stmt->execute([$cateID]);
 $category = $cate_stmt->fetch(PDO::FETCH_ASSOC);
 ?>
- 
-?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asocompass</title>
-
-    <!--fontのリンク-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-    <!--iconのリンク-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="icon" href="../assets/image/favicon.ico">
-    <!--Bootstrap5のリンク-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <!-- PHP関連読み込み -->
-    <?php require 'navbar.php'; ?> <!-- navbarのリンク -->
-    <!--style.cssに書き加えて、cssのファイル名を変更してください-->
-    <link rel="stylesheet" href="../assets/css/category-choice.css">
-    <link rel="stylesheet" href="../assets/css/profile.css">
-</head>
-<body>
       <!--
         Top section
       -->
